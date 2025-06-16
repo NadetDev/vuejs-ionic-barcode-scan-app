a4f02aae5bbd349b06787ddf2062e0da812f8026
NadetDev <nadetdev@gmail.com>
2025-06-16 20:16:48 +0200

    init commit

---
+++ android/app/src/main/java/vuejs/qr/scanner/MainActivity.java
@@ -0,0 +1,5 @@
+package vuejs.qr.scanner;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'vuejs.qr.scanner',
+  appName: 'vuejs-qr-scanner',
+  webDir: 'dist'
+};
+
+export default config;
