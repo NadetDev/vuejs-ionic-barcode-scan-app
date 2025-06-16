b4e0b0c4db7e1076a1ec8f9282d87a961c40758e
NadetDev <nadetdev@gmail.com>
2025-06-16 23:01:23 +0200

    add barcode scan feature

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,71 @@
+<script setup lang="ts">
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { scan } from 'ionicons/icons';
+import { onMounted, ref } from 'vue';
+import {
+  IonPage,
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  alertController,
+  IonFooter,
+  IonFabButton,
+  IonIcon,
+} from '@ionic/vue';
+
+const isSupported = ref(false);
+const barcodeList = ref([] as Barcode[]);
+
+onMounted(async () => {
+  isSupported.value = (await BarcodeScanner.isSupported()).supported;
+});
+
+const startScanning = async () => {
+  const granted = await requestPermissions();
+
+  if (!granted) {
+    presentAlert(
+      'Permission denied',
+      'Please grant camera permission to use the barcode scanner.'
+    );
+    return;
+  }
+
+  const { barcodes } = await BarcodeScanner.scan();
+  barcodeList.value.push(...barcodes);
+};
+
+const requestPermissions = async () => {
+  const { camera } = await BarcodeScanner.requestPermissions();
+  return camera === 'granted' || camera === 'limited';
+};
+
+const presentAlert = async (hdr: string, msg: string) => {
+  const alert = await alertController.create({
+    header: hdr,
+    message: msg,
+    buttons: ['OK'],
+  });
+  await alert.present();
+};
+</script>
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
+    <ion-header>
+      <ion-toolbar
+        ><ion-title>NadetDev Barcode Scanner</ion-title></ion-toolbar
+      ></ion-header
+    >
+    <ion-content>{{ barcodeList }} </ion-content>
+    <ion-footer class="ion-padding">
+      <ion-fab-button
+        style="margin: auto"
+        v-if="isSupported"
+        @click="startScanning()"
+      >
+        <ion-icon :icon="scan"></ion-icon>
+      </ion-fab-button>
+    </ion-footer>
   </ion-page>
 </template>
-
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-</script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
